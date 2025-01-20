64497dc484000ef2488065690293b0b3fbd16d44
alvin <alvin.kz18@gmail.com>
2025-01-20 20:07:50 +0900

    added thicc and spac

--- sketch.js
+++ sketch.js
@@ -1,7 +1,7 @@
 let numPoints; // Points per circle for smoothness
 let numLayers = 10;  // Number of radial layers (terrain depth)
 let baseRadius; // Radius of the innermost circle
-let layerSpacing = 12; // Spacing between layers
+let layerSpacing = 15; // Spacing between layers
 let timeFactor = 0.01; // Speed of animation
 
 let slider, slider2, slider3, slider4;
@@ -59,7 +59,7 @@ function setup() {
   labelbi = createLabelWithStyle('Inner Blue', (width - sliderWidth) / 2 + (2 * sliderWidth) / 3, height - 100, sliderWidth / 3 - 10 + 'px');
 
   // # of points (polygon)
-  slider2 = createSliderWithStyle(3, 50, 50, 1, (width - sliderWidth) / 2, height - 150, sliderWidth + 'px');
+  slider2 = createSliderWithStyle(3, 360, 360, 1, (width - sliderWidth) / 2, height - 150, sliderWidth + 'px');
   createLabelWithStyle('Number of Points', (width - sliderWidth) / 2, height - 150, sliderWidth + 'px');
   
   // Speed
@@ -69,6 +69,14 @@ function setup() {
   // Distortion
   slider4 = createSliderWithStyle(0.1, 1.0, 0.01, 0.001, (width - sliderWidth) / 2, height - 250, sliderWidth + 'px');
   createLabelWithStyle('Noise Scale', (width - sliderWidth) / 2, height - 250, sliderWidth + 'px');
+
+  // Thickness
+  slider5 = createSliderWithStyle(1, 5, 1, 0.01, (width - sliderWidth) / 2, height - 300, sliderWidth + 'px');
+  createLabelWithStyle('Thickness', (width - sliderWidth) / 2, height - 300, sliderWidth + 'px');
+
+  // Spacing
+  slider6 = createSliderWithStyle(10, 50, 15, 1, (width - sliderWidth) / 2, height - 350, sliderWidth + 'px');
+  createLabelWithStyle('Spacing', (width - sliderWidth) / 2, height - 350, sliderWidth + 'px');
 }
 
 function draw() {
@@ -76,12 +84,13 @@ function draw() {
   translate(width / 2, height / 3); // Center the visualization
 
   numPoints = slider2.value();
+  layerSpacing = slider6.value();
 
   // Get the color values for the inner and outer color strokes
   let color1 = color(sliderri.value(), slidergi.value(), sliderbi.value()); // Inner color
   let color2 = color(sliderr.value(), sliderg.value(), sliderb.value()); // Outer color (can be different)
 
-  strokeWeight(1.2);
+  strokeWeight(slider5.value());
   for (let layer = 0; layer < numLayers; layer++) {
     let radius = baseRadius + layer * layerSpacing; // Radius of the current layer
     let noiseScale = layer * slider4.value(); // Scale noise differently for each layer
