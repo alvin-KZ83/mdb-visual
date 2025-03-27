e99fe46d9225fa1fad1296152902121db666b2a8
alvin <alvin.kz18@gmail.com>
2025-03-27 09:20:09 +0900

    added camera function

--- sketch.js
+++ sketch.js
@@ -115,7 +115,7 @@ function draw() {
   text(`${heartRate}`, 0, 0);
 
   if (video) {
-    image(video, 0, 0, 100, 100);
+    image(video, -windowWidth / 2 + 50, -windowHeight / 2 + 50, 3 * 180, 3 * 90); // Tailored to S25
   }
 }
 
@@ -182,6 +182,59 @@ function mouseReleased() {
   swiping = false;
 }
 
-function keyPressed() {
-  connectToBLE()
+document.querySelector('#connectBluetooth').addEventListener('click', e => connectToBLE());
+
+/*
+ *  Copyright (c) 2015 The WebRTC project authors. All Rights Reserved.
+ *
+ *  Use of this source code is governed by a BSD-style license
+ *  that can be found in the LICENSE file in the root of the source
+ *  tree.
+ */
+'use strict';
+
+// Put variables in global scope to make them available to the browser console.
+const constraints = window.constraints = {
+  audio: false,
+  video: true
+};
+
+function handleSuccess(stream) {
+  const video = document.querySelector('video');
+  const videoTracks = stream.getVideoTracks();
+  console.log('Got stream with constraints:', constraints);
+  console.log(`Using video device: ${videoTracks[0].label}`);
+  window.stream = stream; // make variable available to browser console
+  video.srcObject = stream;
 }
+
+function handleError(error) {
+  if (error.name === 'OverconstrainedError') {
+    errorMsg(`OverconstrainedError: The constraints could not be satisfied by the available devices. Constraints: ${JSON.stringify(constraints)}`);
+  } else if (error.name === 'NotAllowedError') {
+    errorMsg('NotAllowedError: Permissions have not been granted to use your camera and ' +
+      'microphone, you need to allow the page access to your devices in ' +
+      'order for the demo to work.');
+  }
+  errorMsg(`getUserMedia error: ${error.name}`, error);
+}
+
+function errorMsg(msg, error) {
+  const errorElement = document.querySelector('#errorMsg');
+  errorElement.innerHTML += `<p>${msg}</p>`;
+  if (typeof error !== 'undefined') {
+    console.error(error);
+  }
+}
+
+async function init(e) {
+  try {
+    const stream = await navigator.mediaDevices.getUserMedia(constraints);
+    handleSuccess(stream);
+    e.target.disabled = true;
+  } catch (e) {
+    handleError(e);
+  }
+}
+
+document.querySelector('#showVideo').addEventListener('click', e => init(e));
